946c10ac052e1b160cdc102352054a5557685faf
myk <1649896696@qq.com>
2021-05-20 09:48:06 +0800

    Initial commit

---
+++ app/src/main/java/com/teacher/publishgithub/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.teacher.publishgithub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.teacher.mylibrary.StringUtils
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
---
+++ mylibrary/src/main/java/com/teacher/mylibrary/StringUtils.kt
@@ -0,0 +1,16 @@
+package com.teacher.mylibrary
+
+/**
+ * 作者: wesley
+ * 日期: 2021/5/20
+ * 描述:
+ */
+object StringUtils {
+
+
+
+
+    fun isMobiles(phoneNumber: String): Boolean{
+        return phoneNumber.length == 11
+    }
+}
